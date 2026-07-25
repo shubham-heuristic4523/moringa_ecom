e1ac9945a3048f516b823f5ffc63666f3bcbf619
Shubham Chhanwal <shubhamc.heuristic@gmail.com>
2026-07-25 11:32:30 +0530

    adding user

---
+++ app/Models/Customer/CustomerProfile.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models\Customer;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class CustomerProfile extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'gender',
+        'date_of_birth',
+        'profile_photo',
+        'company_name',
+        'gst_number',
+        'bio',
+        'profile_completed',
+    ];
+
+    protected $casts = [
+        'date_of_birth' => 'date',
+        'profile_completed' => 'boolean',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Providers;
-
+use App\Models\User;
+use App\Observers\UserObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        User::observe(UserObserver::class);
     }
 }
